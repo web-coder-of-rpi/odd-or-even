6b489fbc8f1ac5fee31fb635b73eb90ee0122a45
web-coder-of-rpi <leo.jarnicki@icloud.com>
2025-10-28 15:52:46 +0800

    Fix function name from odd_or_even to oddoreven

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import odd_or_even
 
-print(odd_or_even(4))
-print(odd_or_even(5))
-print(odd_or_even(55527534))
-print(odd_or_even(64767543))
+print(oddoreven(4))
+print(oddoreven(5))
+print(oddoreven(55527534))
+print(oddoreven(64767543))
